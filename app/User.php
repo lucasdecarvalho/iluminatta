c0d56042c493bb40f09fdc729d568390612d8511
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2020-08-26 14:39:33 -0300

    fix user column

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'name', 'lastname', 'doc', 'phone1', 'phone2', 'email', 'password', 'is_admin', 'address_title', 'address', 'number', 'comp', 'neigh', 'city', 'state', 'zipcode', 'country',
+        'id', 'name', 'lastname', 'doc', 'phone1', 'phone2', 'email', 'password', 'is_admin', 'address_title', 'street', 'number', 'comp', 'neigh', 'city', 'state', 'zipcode', 'country',
     ];
 
     /**
