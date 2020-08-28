ec229bd70229a00ac4d595bbf858c1747fea5f40
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2020-08-28 17:47:12 -0300

    model warning to delete product

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -35,14 +35,40 @@
                     <a class="btn btn-info" href="{{ route('admin.show',$admin->id) }}">Detalhes</a>
     
                     <a class="btn btn-primary" href="{{ route('admin.edit',$admin->id) }}">Editar</a>
+                    <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#DeleteModal">Deletar</a>
    
                     @csrf
                     @method('DELETE')
       
-                    <button type="submit" class="btn btn-danger">Deletar</button>
+                    <!-- <button type="submit" class="btn btn-danger">Deletar</button> -->
                 </form>
             </td>
         </tr>
+        <!-- Delete Modal-->
+        <div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Atenção!</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                </div>
+                <div class="modal-body">Deseja realmente DELETAR esse produto?</div>
+                <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+
+                <form action="{{ route('admin.destroy',$admin->id) }}" method="POST">
+        
+                    @csrf
+                    @method('DELETE')
+        
+                    <button class="btn btn-danger" type="submit" >Deletar</button>
+                </form>
+                </div>
+            </div>
+            </div>
+        </div>
         @endforeach
     </table>
 
