e06044be6a1b65c5477a114032066dac07eb56c9
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2020-09-03 13:29:19 -0300

    layout update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,16 +37,10 @@
             font-size:32px;
             color:#fff;
             line-height:0;
-            margin-top:1.1em;
+            margin-top:.8em;
         }
         .slide {
             overflow:hidden;
-        }
-        @media (min-width: 800px)
-        {
-            #menuItens {
-                top:40px;
-            }
         }
 
         input::-webkit-outer-spin-button,
@@ -59,152 +53,152 @@
         }
     </style>
 </head>
-<body>
-    <div class="flex-center position-ref full-height">
+<body style="">
+    <div class="flex-center bg-white">
+        <!-- top warning -->
+        <!-- removed -->
         <!-- header -->
-        <div class="navbar p-1 d-none text-center" style="background:#b08bbe;">
-            <span class="w-100 text-light">Indique-nos a uma amiga ou amigo e ganhe 25% de desconto em sua próxima compra. Código: FRIENDS25</span>
-            </div>
-            <nav class="navbar sticky-top navbar-expand-md navbar-dark d-none d-md-block pt-0 pb-0" style="background:#2c1b47;border-bottom: solid 1px rgba(157, 111, 174, .3);">
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">WhatsApp: (19) 1234-5678 <span class="sr-only">(current)</span></a>
-                    </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#">Localidade da Loja</a>
-                    </li> -->
-                    <li class="nav-item">
-                        <!-- <a class="nav-link" href="../client">Inscreva-se em nossa Newsletter</a> -->
-                    </li>
-                </ul>
-                <span class="navbar-text p-0">
-                    <ul class="navbar-nav mr-auto">
+        <!-- top menu contact, cart -->
+        <header class="col-12 p-0 m-0">
+            <nav class="navbar navbar-dark d-none d-xl-block" style="background:#2c1b47;border-bottom: solid 1px rgba(157, 111, 174, .3);">
+                <div class="row">
+                    <ul class="navbar mr-auto p-0 m-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="/client">@if (Auth::check()) {{ "Olá, ". Auth::user()->name }} @else Login / Registro @endif</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cart.index') }}">Carrinho</a>
+                            <a class="nav-link text-light" href="#">WhatsApp: (19) 1234-5678</a>
                         </li>
                     </ul>
-                </span>
-            </div>
+                    <ul class="navbar p-0 m-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="/client">@if (Auth::check()) {{ "Olá, ". Auth::user()->name }} @else Login / Registro @endif</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('cart.index') }}">Carrinho</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-            <div class="navbar navbar-light justify-content-between d-none d-md-block p-md-0" style="background:#2c1b47">
-            <div class="row">
-                <div class="col-4">
-                    <!--  -->
+            <!-- Menu logo search -->
+            <nav class="navbar navbar-light d-none d-xl-block" style="background:#2c1b47">
+                <div class="row">
+                    <div class="col-4 d-none d-xl-block">
+                        <!--  -->
+                    </div>
+                    <div class="col-12 col-xl-4 text-center">
+                        <a href="/"><h1>Iluminatta</h1><i class="fas fa-seedling" style="position:absolute;margin-left:-0em;margin-top:-2em;font-size:1.2em;color:#ff7700;"></i></a>
+                    </div>
+                    <div class="col-4 d-none d-xl-block">
+                        <form class="form-inline" action="{{ route('search.word') }}" method="GET">
+                            <input type="search" name="keyword" class="form-control mr-2 w-75" placeholder="Buscar por produtos na loja" value="@if(isset($keyword)) {{$keyword}} @endif" aria-label="Search">
+                            <button class="btn btn-outline-light" type="submit">Buscar</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="col-md-4 col-12 text-center">
-                    <a href="/"><h1>Iluminatta</h1><i class="fas fa-seedling" style="position:absolute;margin-left:-0em;margin-top:-2em;font-size:1.2em;color:#ff7700;"></i></a>
+            </nav>
+            <!-- top menu -->
+            <nav class="navbar navbar-expand-xl navbar-dark" style="background:#2c1b47">
+                <!-- responsivo -->
+                <div class="col-12 d-block d-xl-none">
+                    <div class="row">
+                        <div class="col-9">
+                            <a href="/"><h1>Iluminatta</h1><i class="fas fa-seedling" style="position:absolute;margin-left:4.45em;margin-top:-1.9em;font-size:1.2em;color:#ff7700;"></i></a>
+                        </div>
+                        <div class="col-3">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarText" aria-expanded="true" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4">
-                    <form action="{{ route('search.word') }}" method="GET" class="form-inline mt-3 float-right w-75">
-                        
-                        <input type="search" name="keyword" class="form-control mr-sm-2 w-75" placeholder="Buscar por produtos na loja" value="@if(isset($keyword)) {{$keyword}} @endif" aria-label="Search">
-                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
-                    </form>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+                    <ul class="navbar-nav">
+                        <div class="col-12 p-2 mt-3 mb-2 rounded bg-dark">
+
+                            <li class="nav-item d-block d-xl-none border-bottom p-2">
+                                <a class="nav-link text-light" href="/client"><i class="fas fa-user"></i> @if (Auth::check()) {{ "Olá, ". Auth::user()->name }} @else Login / Registro @endif</a>
+                            </li>
+                            <li class="nav-item d-block d-xl-none p-2">
+                                <a class="nav-link text-light" href="{{ route('cart.index') }}"><i class="fas fa-shopping-cart"></i> Carrinho</a>
+                            </li>
+                            
+                        </div>
+                        @foreach($categories as $catg)
+                        <li>
+                            <a class="nav-item nav-link text-light" href="{{ route('shop.index',$catg->path ?? null) }}">{{ $catg->title ?? null }}</a>
+                        </li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
-            </div>
-            <div class="navbar sticky-top navbar-expand-md navbar-dark pt-0 pb-0 m-0" id="menuItens" style="background:#2c1b47;">
-            <div class="row w-100 p-0 m-0 d-md-none d-block">
-                <div class="col-9 d-md-none d-block float-left">
-                        <a href="/"><h1 style="margin-top:1em;">Iluminatta</h1><i class="fas fa-seedling" style="position:absolute;margin-left:4.45em;margin-top:-1.9em;font-size:1.2em;color:#ff7700;"></i></a>
-                </div>
-                <div class="col-3 float-right mt-2 mb-2">
-                    <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarText" aria-expanded="true" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-            </div>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-                <ul class="navbar-nav">
-                    <li class="nav-item d-block d-xl-none">
-                        <a class="nav-link" href="../client">@if (Auth::check()) {{ "Olá, ". Auth::user()->name }} @else Login / Registro @endif</a>
-                    </li>
-                    <li class="nav-item border-bottom d-block d-xl-none">
-                        <a class="nav-link" href="{{ route('cart.index') }}">Carrinho</a>
-                    </li>
-                    @foreach ($categories as $catg)
-                    <li><a class="nav-item nav-link" href="{{ route('shop.index',$catg->path ?? null) }}">{{ $catg->title ?? null }}</a></li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
+            </nav>
+        </header>
         <!-- header end -->
 
         @yield('content')
         
         <!-- Footer -->
-        <footer class="text-light" style="background:#2c1b47;">
-            <div class="footer-head bg-dark">
-            <!-- <div class="footer-head bg-secondary" style="background:#222;border-bottom: solid 1px rgba(157, 111, 174, .3);"> -->
-                <div class="container pt-4">
+        <div class="footer-head bg-dark text-light">
+            <div class="col-12">
+                <div class="container pt-4 pb-4">
                     <div class="row">
-                        <div class="col-sm">
-                        <div class="row">
-                            <form action="{{ route('newsletter.store') }}" method="POST" class="form-inline mb-3 w-100 w-xl-75">
-                                @csrf
-                                <div class="form-row">
-                                    <input class="form-control mr-2" type="text" name="name" placeholder="Digite seu Nome" required>
-                                    <input class="form-control mr-2" type="text" name="email" placeholder="Digite seu E-mail" required>
-                                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Assinar</button>
-                                </div>
-                            </form>
+                        <div class="col-12 col-xl-6">
+                            <div class="row">
+                                <form action="{{ route('newsletter.store') }}" method="POST" class="form-inline mb-3 w-100 w-xl-75">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" name="name" placeholder="Digite seu Nome" required>
+                                        <input class="form-control" type="text" name="email" placeholder="Digite seu E-mail" required>
+                                        <button class="btn btn-outline-light" type="submit">Assinar</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="row">
+                                <span>Inscreva-se em nossa newsletter e receba mensagens periodicamente sobre promoções, novidades e destaques de nossa loja.</span>
+                            </div>
                         </div>
-                        <div class="row">
-                            <span>Inscreva-se em nossa newsletter e receba mensagens periodicamente sobre promoções, novidades e destaques de nossa loja.</span>
-                        </div>
-                        </div>
-                        <div class="col-sm">
-                        <div class="row">
-                            <ul class="mt-3" style="font-size:3rem;">
-                                <li class="float-left m-2"><a class="text-light" href="https://www.facebook.com/Iluminatta-654918827851954/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
-                                <!-- <li class="float-left m-2"><a class="text-light" href="http://" target="_blank"><i class="fab fa-twitter-square"></i></a></li> -->
-                                <li class="float-left m-2"><a class="text-light" href="https://www.instagram.com/iluminattastore/" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                                <!-- <li class="float-left m-2"><a class="text-light" href="http://" target="_blank"><i class="fab fa-youtube"></i></a></li> -->
-                            </ul>
-                        </div>
+                        <div class="col-12 col-xl-6">
+                            <div class="row">
+                                <ul class="mt-3" style="font-size:2rem;">
+                                    <li class="float-left m-2"><a class="text-light" href="https://www.facebook.com/Iluminatta-654918827851954/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                                    <!-- <li class="float-left m-2"><a class="text-light" href="http://" target="_blank"><i class="fab fa-twitter-square"></i></a></li> -->
+                                    <li class="float-left m-2"><a class="text-light" href="https://www.instagram.com/iluminattastore/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                    <!-- <li class="float-left m-2"><a class="text-light" href="http://" target="_blank"><i class="fab fa-youtube"></i></a></li> -->
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="container p-4">
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="row">
-                        <h4>Sobre</h4>
-                        <p>Somos uma loja de produtos importados, com ótimo estoque e custo benefício. Navegue por nossas páginas e encontre os produtos que mais tem a ver com o seu estilo. Temos diversas formas de pagamento e facilidades.</p>
+        </div>
+        <footer class="text-light" style="background:#2c1b47;">
+            <div class="col-12">
+                <div class="container pt-4 pb-4">
+                    <div class="row">
+                        <div class="col-12 col-xl-8">
+                            <div class="row">
+                                <h4>Sobre</h4>
+                                <p>Somos uma loja de produtos importados, com ótimo estoque e custo benefício. Navegue por nossas páginas e encontre os produtos que mais tem a ver com o seu estilo. Temos diversas formas de pagamento e facilidades.</p>
+                            </div>
                         </div>
-                        <div class="row">
-
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <div class="row">
-                        <div class="col">
-                            <ul>
-                                <li><a class="text-light" href="http://">Contato</a></li>
-                                <li><a class="text-light" href="http://">FAQ</a></li>
-                                <li><a class="text-light" href="http://">Meus Pedidos</a></li>
-                                <li><a class="text-light" href="http://">Política de retorno</a></li>
-                            </ul>
-                        </div>
-                        <div class="col">
-                            <ul>
-                                <li><a class="text-light" href="http://">Informações de compra</a></li>
-                                <li><a class="text-light" href="http://">Privacidade</a></li>
-                                <li><a class="text-light" href="http://">Termos de uso</a></li>
-                                <li><a class="text-light" href="http://">Permissão de uso</a></li>
-                            </ul>
-                        </div>
+                        <div class="col-12 col-xl-4">
+                            <div class="row">
+                                <ul class="col-12 col-xl-6">
+                                    <li><a class="text-light" href="http://">Contato</a></li>
+                                    <li><a class="text-light" href="http://">FAQ</a></li>
+                                    <li><a class="text-light" href="http://">Meus Pedidos</a></li>
+                                    <li><a class="text-light" href="http://">Política de retorno</a></li>
+                                </ul>
+                                <ul class="col-12 col-xl-6">
+                                    <li><a class="text-light" href="http://">Informações de compra</a></li>
+                                    <li><a class="text-light" href="http://">Privacidade</a></li>
+                                    <li><a class="text-light" href="http://">Termos de uso</a></li>
+                                    <li><a class="text-light" href="http://">Permissão de uso</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm pt-4 text-center" style="border-top: solid 1px rgba(157, 111, 174, .3);">
-                        <p>© 2020 Iluminatta. Todos os direitos reservados.</p>
+                    <div class="row">
+                        <div class="col-12 pt-4 text-center" style="border-top: solid 1px rgba(157, 111, 174, .3);">
+                            <p class="p-0 m-0">© 2020 Iluminatta. Todos os direitos reservados.</p>
+                        </div>
                     </div>
                 </div>
             </div>
